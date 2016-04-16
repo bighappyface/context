874988f99a0080195bff9f3def320f32591cf04d
bighappyface <anomalous20@gmail.com>
2016-04-16 12:00:00 -0500

    test(unit): add Unit/Entity/ContextTest

---
+++ tests/src/Unit/Entity/ContextTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\Tests\context\Unit\Entity\ContextTest.
+ */
+
+namespace Drupal\Tests\context\Unit\Entity;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\context\Entity\Context;
+
+/**
+ * Tests Entity\Context methods.
+ *
+ * @group contextb
+ */
+class ContextTest extends UnitTestCase {
+
+  /**
+   * Test implements.
+   */
+  public function testImplements() {
+    $this->assertTrue(TRUE);
+  }
+
+}
